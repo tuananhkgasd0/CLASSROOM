9176cad71c6fd6a31fdbc55e63350b9047ef4758
Hoang Phuc <thienloc1220@gmail.com>
2022-01-13 14:28:56 +0700

    Add URL of public host

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -6,7 +6,7 @@ import queryString from "query-string";
 // Please have a look at here `https://github.com/axios/axios#request-config` for the full list of configs
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     "content-type": "application/json",
   },
